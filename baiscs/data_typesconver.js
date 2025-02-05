0f686ca8aa363cb42d3b9acc6efd442b848a8952
Prachi <117566542+achieverprachi@users.noreply.github.com>
2025-02-05 15:08:56 +0000

    Minor data changes

--- baiscs/data_typesconver.js
+++ baiscs/data_typesconver.js
@@ -1,5 +1,32 @@
 let score = "33"
-console.og (typeof score);
+console.log (typeof score);
 console.log (typeof(score));
 let valueInNumber = Number (score)
 console.log (typeof valueInNumber);
+
+
+
+let scores = "334vbsf"
+console.log (typeof scores);
+console.log (typeof(scores));
+let valueInNumbers = Number (scores)
+console.log (typeof valueInNumbers);
+console.log(valueInNumbers) // will come out ot be NaN
+
+// null mein 0 aayega 
+// unassigned mein NaN aayega 
+// in bboelan true-1 , flase = 0
+
+
+let isLogged = 1; // in bool it will correspond to true
+let boolIsLogged = Boolean(isLogged)
+console.log(boolIsLogged)
+
+// "' => false
+// "hitesh" →> true
+
+
+
+let stringNumber = String (someNumber)
+console.log (stringNumber);
+console.log (typeof stringNumber);
