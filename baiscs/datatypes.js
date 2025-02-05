46532bbf606d7ff5fc71dab928a3f4c9a3e4042e
Prachi <117566542+achieverprachi@users.noreply.github.com>
2025-02-05 14:41:25 +0000

    Conatins files

---
+++ baiscs/datatypes.js
@@ -0,0 +1,9 @@
+"use strict";
+//the bewlo will show error sionce we are suing node
+// the below works in browser ,, can try in the inspect->console
+// alert(3+3)
+// java script ka enegine brwoser mein chhupa huah 
+
+console. log (3
+    +
+    3)
---
+++ baiscs/variables.js
@@ -0,0 +1,15 @@
+const acc_id = "123445"
+let name = "Prachi"
+var email = "abc@gmail.com"
+city = "delhi"
+let state
+// if nothing assigned it is unassigned printed
+
+city="karnal"
+email= "aws@gmail.com "
+name = "Rachit"
+
+console.table([acc_id , name , email , city]);
+
+// dont use var , because of lissue in lack of functional and block scope 
+///  use let
