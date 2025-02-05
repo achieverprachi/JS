4f949d41f12ab024925add59c3a3948e23884f1a
Prachi <117566542+achieverprachi@users.noreply.github.com>
2025-02-05 15:23:27 +0000

    More small chnage

---
+++ baiscs/dt_operations.js
@@ -0,0 +1,46 @@
+let value = 3
+34
+let negValue = -value
+console. log (negValue);
+
+console.log (2+2); 
+console.log (2-2); 
+console.log (2*2); 
+console.log (2**3);
+console.log (2/3); 
+console.log (2%3);
+
+
+
+let str1 = "hello"
+let str2 = " prachi"
+let str3 = str1 + str2
+console.log(str3);
+
+
+console. log ("1" + 2);
+console. log (1 + "2");
+console. log ("1" + 2 + 2);
+console. log (1 + 2 + "2");
+
+console. log(+true); // here the +tue will give output as 1
+// but true= is erronorus
+
+console. log (+""); // will give output 0
+
+let gameCounter = 100
+gameCounter++;
+console. log (gameCounter);
+
+/////// prefix vs postfiv
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
