9c7e6503bbb445a028a0a94c0af398614f0d3aff
Prachi <117566542+achieverprachi@users.noreply.github.com>
2025-02-05 15:00:09 +0000

    SOme minor chnages

--- baiscs/datatypes.js
+++ baiscs/datatypes.js
@@ -7,3 +7,22 @@
 console. log (3
     +
     3)
+
+
+// number = 2 to power 53
+// bigint
+// string => '
+// boolean => true/false
+// null = standalone value
+// undefined =>
+//symbol => unique    
+    
+    
+    // type of null is object
+    // type of undefined isundefined itslef
+
+    console. log (typeof undefined); // undefined 
+    console. log (typeof null); // object 
+
+
+
